26e2dbdbcaccc59670663166361b769c8a7418ce
Venelin Bakalov <venelin_bakalov@abv.bg>
2016-09-28 01:29:01 +0300

    added more exercises to folder

---
+++ Data-Types-and-Variables-Exercises/IntegerToHexAndBinary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerToHexAndBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string numberHexadecimal = Convert.ToString(number, 16).ToUpper();
+            string numberBinary = Convert.ToString(number, 2).ToUpper();
+            Console.WriteLine(numberHexadecimal);
+            Console.WriteLine(numberBinary);
+        }
+    }
+}
---
+++ Data-Types-and-Variables-Exercises/PracticeCharsAndStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeCharsAndStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string softUni = "Software University";
+            char charFirst = 'B';
+            char charSecond = 'y';
+            char charThird = 'e';
+            string sentence = "I love programming";
+
+            Console.WriteLine(softUni);
+            Console.WriteLine(charFirst);
+            Console.WriteLine(charSecond);
+            Console.WriteLine(charThird);
+            Console.WriteLine(sentence);
+        }
+    }
+}
