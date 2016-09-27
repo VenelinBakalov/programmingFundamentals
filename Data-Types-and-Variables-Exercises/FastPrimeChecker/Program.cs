ed4c3181f13753c3b2e1e1062577f9597e2b051b
Venelin Bakalov <venelin_bakalov@abv.bg>
2016-09-28 01:26:39 +0300

    added missing files to FastPrimeChecker

---
+++ Data-Types-and-Variables-Exercises/FastPrimeChecker/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastPrimeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int range = int.Parse(Console.ReadLine());
+            for (int number = 2; number <= range; number++)
+            {
+                bool isPrime = true;
+                for (int numberCheck = 2; numberCheck <= Math.Sqrt(number); numberCheck++)
+                {
+                    if (number % numberCheck == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine($"{number} -> {isPrime}");
+            }
+
+        }
+    }
+}
