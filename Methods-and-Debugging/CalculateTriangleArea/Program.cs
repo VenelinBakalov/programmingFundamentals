9c291f7ca78bf51340e944ee781f60d5768cbef9
Venelin Bakalov <venelin_bakalov@abv.bg>
2016-09-29 00:56:53 +0300

    added "Methods-and-Debugging" folder

---
+++ Methods-and-Debugging/CalculateTriangleArea/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateTriangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double area = GetTriangleArea(width, height);
+            Console.WriteLine(area);
+        }
+
+        static double GetTriangleArea(double width, double height)
+        {
+            double triangleArea = width * height / 2;
+            return triangleArea;
+        }
+    }
+}
---
+++ Methods-and-Debugging/DrawAFilledSquare/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawAFilledSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintSquare();
+        }
+
+        static void PrintSquare()
+        {
+            int input = int.Parse(Console.ReadLine());
+            PrintEnd(input);
+            for (int i = 0; i < input - 2; i++)
+            {
+            PrintMiddle(input);
+            }
+            PrintEnd(input);
+        }
+
+        static void PrintEnd(int input)
+        {
+            Console.WriteLine(new string('-', 2 * input));
+        }
+
+        static void PrintMiddle(int input)
+        {
+            Console.Write('-');
+            for (int i = 0; i < input - 1; i++)
+            {
+                Console.Write("\\/");
+            }
+            Console.WriteLine('-');
+        }
+    }
+}
