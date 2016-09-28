4ee7deee897c90b2e6ad75c8d12c2b4bd2c7e308
Venelin Bakalov <venelin_bakalov@abv.bg>
2016-09-29 00:58:22 +0300

    added more files

---
+++ Methods-and-Debugging/PrintingTriangle/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintingTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintTriangle();
+        }
+
+        static void PrintTriangle()
+        {
+            int input = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= input; i++)
+            {
+                PrintLine(1, i);
+            }
+
+            for (int i = input - 1; i >= 1; i--)
+            {
+                PrintLine(1, i);
+            }
+        }
+
+        static void PrintLine(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
