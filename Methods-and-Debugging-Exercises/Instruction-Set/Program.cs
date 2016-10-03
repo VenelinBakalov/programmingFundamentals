b4d9a392c16d961df2d13d4180f597fbc21f8b0c
Venelin Bakalov <venelin_bakalov@abv.bg>
2016-10-03 10:32:38 +0300

    added "Instruction-Set.csproj"

---
+++ Methods-and-Debugging-Exercises/Instruction-Set/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class InstructionSet_broken
+{
+    static void Main()
+    {
+        string opCode = Console.ReadLine().ToLower();
+
+        while (opCode != "end")
+        {
+            string[] codeArgs = opCode.Split(' ');
+            long result = 0;
+            switch (codeArgs[0])
+            {
+                case "inc":
+                    {
+                        int operandOne = int.Parse(codeArgs[1]);
+                        result = operandOne++;
+                        break;
+                    }
+                case "dec":
+                    {
+                        int operandOne = int.Parse(codeArgs[1]);
+                        result = operandOne--;
+                        break;
+                    }
+                case "add":
+                    {
+                        int operandOne = int.Parse(codeArgs[1]);
+                        int operandTwo = int.Parse(codeArgs[2]);
+                        result = operandOne + operandTwo;
+                        break;
+                    }
+                case "mla":
+                    {
+                        int operandOne = int.Parse(codeArgs[1]);
+                        int operandTwo = int.Parse(codeArgs[2]);
+                        result = (long)(operandOne * operandTwo);
+                        break;
+                    }
+            }
+
+            Console.WriteLine(result);
+            opCode = Console.ReadLine();
+            codeArgs = opCode.Split(' ');
+        }
+    }
+}
