65ab958fa9b0e608fba152dbeed0471f5926b6a5
venelinbakalov <venelin_bakalov@abv.bg>
2016-10-04 00:44:56 +0300

    added "Append-Lists.csproj"

---
+++ Arrays-and-Lists/Append-Lists/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Append_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split('|');
+
+            List<int> results = new List<int>();
+
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                string[] singleArrayAsString = input[i].Split(' ');
+
+                for (int j = 0; j < singleArrayAsString.Length; j++)
+                {
+                    if (singleArrayAsString[j] != "")
+                    {
+                    results.Add(int.Parse(singleArrayAsString[j]));
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", results));
+        }
+    }
+}
