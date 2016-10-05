976021225f7079c799dd0bf406d12e227666a9a4
venelinbakalov <venelin_bakalov@abv.bg>
2016-10-05 12:24:41 +0300

    added "Square-Numbers/Program.cs"

---
+++ Arrays-and-Lists/Square-Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            List<int> numbers = new List<int>();
+            foreach (string number in input)
+            {
+                numbers.Add(int.Parse(number));
+            }
+
+            List<int> squares = new List<int>();
+
+            foreach (int number in numbers)
+            {
+                if (Math.Sqrt(number) == (int)Math.Sqrt(number))
+                {
+                    squares.Add(number);
+                }
+            }
+
+            squares.Sort((a, b) => b.CompareTo(a));
+
+            Console.WriteLine(string.Join(" ", squares));
+        }
+    }
+}
