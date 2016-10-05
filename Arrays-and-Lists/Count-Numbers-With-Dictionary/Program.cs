94b55a9b5960ea1a29796761840b555655d582c2
venelinbakalov <venelin_bakalov@abv.bg>
2016-10-06 00:07:05 +0300

    "Arrays-and-Lists.sln" finished

---
+++ Arrays-and-Lists/Count-Numbers-With-Dictionary/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_Numbers_With_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            SortedDictionary<int, int> count = new SortedDictionary<int, int>();
+
+            foreach (int number in numbers)
+            {
+                if (count.ContainsKey(number))
+                {
+                    count[number]++;
+                }
+                else
+                {
+                    count[number] = 1;
+                }
+            }
+
+            foreach (KeyValuePair<int, int> pair in count)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+    }
+}
---
+++ Arrays-and-Lists/Count-Numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            List<int> numbers = new List<int>();
+
+            foreach (string number in input)
+            {
+                numbers.Add(int.Parse(number));
+            }
+
+            numbers.Sort();
+
+            int position = 0;
+
+            while (position < numbers.Count)
+            {
+                int number = numbers[position];
+                int count = 1;
+                while (position + count < numbers.Count && numbers[position + count] == number)
+                {
+                    count++;
+                }
+                position += count;
+                Console.WriteLine($"{number} -> {count}");
+            }
+        }
+    }
+}
+
