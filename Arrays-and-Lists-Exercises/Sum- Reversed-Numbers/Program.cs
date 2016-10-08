18371f2ddb770fffe41507889928e1527030c3e5
venelinbakalov <venelin_bakalov@abv.bg>
2016-10-09 02:18:13 +0300

    added Sum- Reversed-Numbers.csproj

---
+++ Arrays-and-Lists-Exercises/Sum- Reversed-Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum__Reversed_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int reversed = 0;
+                while (numbers[i] > 0)
+                {
+                    int lastDigit = numbers[i] % 10;
+                    reversed = reversed * 10 + lastDigit;
+                    numbers[i] /= 10;
+                }
+                sum += reversed;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Arrays-and-Lists-Exercises/Sum-Reversed-Numbers-New/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_Reversed_Numbers_New
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> numbers = Console.ReadLine().Split().ToList();
+            List<int> reversedNumbers = new List<int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                string number = numbers[i];
+                string reversedNumbersAsString = StringReverse(number);
+                int reversedNumber = int.Parse(reversedNumbersAsString);
+                reversedNumbers.Add(reversedNumber);
+            }
+
+            int sum = reversedNumbers.Sum();
+            Console.WriteLine(sum);
+
+        }
+
+        private static string StringReverse(string number)
+        {
+            char[] array = number.ToCharArray();
+            char[] reversedArray = array.Reverse().ToArray();
+            string reversedString = string.Join("", reversedArray);
+            return reversedString;
+        }
+    }
+}
