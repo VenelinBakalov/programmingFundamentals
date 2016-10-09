32b26fb935b59d505e8948f51b580dcc14d1df76
Venelin Bakalov <venelin_bakalov@abv.bg>
2016-10-10 00:40:32 +0300

    added Index-of-Letters-Array.csproj 3 different solutions to Arrays-and-Lists-Exercises/Arrays-and-Lists-Exercises.sln

---
+++ Arrays-and-Lists-Exercises/Index-of-Letters-Array/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Index_of_Letters_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            char[] arr = new char[26];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = (char)('a' + i);
+            }
+            for (int i = 0; i < word.Length; i++)
+            {
+                Console.WriteLine(word[i] + " -> " + Array.IndexOf(arr, word[i]));
+            }
+        }
+    }
+}
---
+++ Arrays-and-Lists-Exercises/Index-of-Letters/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Index_of_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] letters = Console.ReadLine().ToCharArray();
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+                Console.WriteLine("{0} -> {1}", letters[i], (int)letters[i] - 97);
+            }
+        }
+    }
+}
