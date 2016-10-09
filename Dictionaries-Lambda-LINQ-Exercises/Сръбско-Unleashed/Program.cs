717727c25989a25f38cec7fbd0d2a2bcfd506791
venelinbakalov <venelin_bakalov@abv.bg>
2016-10-09 20:58:32 +0300

    added more code to сръбско-unleashed

--- Dictionaries-Lambda-LINQ-Exercises/Сръбско-Unleashed/Program.cs
+++ Dictionaries-Lambda-LINQ-Exercises/Сръбско-Unleashed/Program.cs
@@ -10,45 +10,82 @@ namespace Сръбско_Unleashed
     {
         static void Main(string[] args)
         {
-            char[] splitters = { ' ', '@' };
+            Dictionary<string, Dictionary<string, long>> cityProfit = new Dictionary<string, Dictionary<string, long>>();
+            
             string[] singerInfo = Console.ReadLine().Split('@').ToArray();
-
-            List<string> cityName = new List<string>();
-            int moneyMade = 0;
 
             while (!singerInfo[0].Equals("End"))
             {
-                string[] singer = singerInfo[0].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string[] singerArr = singerInfo[0].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
                 string[] city = singerInfo[1].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string singer = string.Join(" ", singerArr);
 
-                bool isCity = true;
-
-                while (isCity)
+                for (int i = 0; i < city.Length; i++)
                 {
-                    for (int i = 0; i < city.Length; i++)
+                    try
                     {
-                        try
+                        long profit = GetProfit(city[i], city[i + 1]);
+
+                        List<string> cityName = new List<string>();
+                        for (int j = 0; j < i; j++)
                         {
-                            int ticketPrice = int.Parse(city[i]);
-                            int ticketsCount = int.Parse(city[i + 1]);
-                            for (int j = 0; j < i; j++)
-                            {
-                                cityName.Add(city[j]);
-                            }
-                            moneyMade = ticketPrice * ticketsCount;
-                            isCity = false;
-                            break;
+                            cityName.Add(city[j]);
                         }
-                        catch (Exception)
-                        {
-                            isCity = true;
-                        }
+
+                        string newCity = string.Join(" ", cityName);
+
+                        InsertCity(cityProfit, newCity);
+                        InsertProfit(cityProfit, newCity, singer, profit);
+
+                        break;
+                    }
+                    catch (Exception)
+                    {
                     }
                 }
                 singerInfo = Console.ReadLine().Split('@').ToArray();
             }
-            Console.WriteLine(string.Join(" ", cityName));
-            Console.WriteLine(moneyMade);
+
+            PrintProfit(cityProfit);
+
+        }
+
+        private static void PrintProfit(Dictionary<string, Dictionary<string, long>> cityProfit)
+        {
+            foreach (KeyValuePair<string, Dictionary<string, long>> cityEntry in cityProfit)
+            {
+                Console.WriteLine(cityEntry.Key);
+
+                foreach (KeyValuePair<string, long> singerProfit in cityEntry.Value.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine($"#  {singerProfit.Key} -> {singerProfit.Value}");
+                }
+            }
+        }
+
+        private static long GetProfit(string v1, string v2)
+        {
+            long ticketPrice = long.Parse(v1);
+            long ticketsCount = long.Parse(v2);
+            long profit = ticketPrice * ticketsCount;
+            return profit;
+        }
+
+        private static void InsertProfit(Dictionary<string, Dictionary<string, long>> cityProfit, string newCity, string singer, long profit)
+        {
+            if (!cityProfit[newCity].ContainsKey(singer))
+            {
+                cityProfit[newCity].Add(singer, 0);
+            }
+            cityProfit[newCity][singer] += profit;
+        }
+
+        private static void InsertCity(Dictionary<string, Dictionary<string, long>> cityProfit, string newCity)
+        {
+            if (!cityProfit.ContainsKey(newCity))
+            {
+                cityProfit.Add(newCity, new Dictionary<string, long>());
+            }
         }
     }
 }
