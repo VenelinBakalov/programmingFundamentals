b1d019bec7d1e2c454498acd0e0b4f55744e8fde
venelinbakalov <venelin_bakalov@abv.bg>
2016-10-11 01:41:35 +0300

    added Objects-and-Classes-Lab.sln

---
+++ Objects-and-Classes-Lab/Big-Factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Big_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            BigInteger bigNumber = 1;
+
+            for (int i = 2; i <= number; i++)
+            {
+                bigNumber *= i;
+            }
+
+            Console.WriteLine(bigNumber);
+        }
+    }
+}
