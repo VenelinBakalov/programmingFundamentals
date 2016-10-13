e8130be63512fe2a77572771ed9c0557b86081b4
venelinbakalov <venelin_bakalov@abv.bg>
2016-10-14 02:46:14 +0300

    added Average-Grades.csproj solution to Objects-and-Classes-Exercises.sln

---
+++ Objects-and-Classes-Exercises/Average-Grades/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Average_Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            int studentCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                string[] commandArgs = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string name = commandArgs[0];
+                List<double> grades = commandArgs.Skip(1).Select(x => double.Parse(x)).ToList();
+
+                Student student = new Student(name);
+                student.Grades.AddRange(grades);
+                students.Add(student);
+            }
+
+            var orderedStudents = students.Where(s => s.Grades.Average() >= 5).OrderBy(s => s.Name)
+                                    .ThenByDescending(s => s.Grades.Average());
+
+            foreach (var student in orderedStudents)
+            {
+                Console.WriteLine($"{student.Name} -> {student.Grades.Average():f2}");
+            }
+        }
+    }
+
+    class Student
+    {
+        public Student(string name)
+        {
+            this.Name = name;
+            this.Grades = new List<double>();
+        }
+        public string Name { get; set; }
+        public List<double> Grades { get; set; }
+    }
+}
