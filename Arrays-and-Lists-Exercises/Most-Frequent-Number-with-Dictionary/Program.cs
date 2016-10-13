0235a7e8cd17412dcc1161424bb91cfef990cf47
venelinbakalov <venelin_bakalov@abv.bg>
2016-10-13 19:29:59 +0300

    added Most-Frequent-Number-with-Dictionary.csproj - new solution with dictionary. Arrays-and-Lists-Exercises/Arrays-and-Lists-Exercises.sln

---
+++ Arrays-and-Lists-Exercises/Most-Frequent-Number-with-Dictionary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Most_Frequent_Number_with_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            Dictionary<string, int> count = new Dictionary<string, int>();
+            foreach (string number in input)
+            {
+                if (!count.ContainsKey(number))
+                {
+                    count.Add(number, 0);
+                }
+                count[number]++;
+            }
+
+            string result = count.OrderByDescending(num => num.Value).FirstOrDefault().Key;
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
