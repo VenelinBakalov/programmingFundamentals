f04cf83f2273098a920f8a9568664dc971287298
venelinbakalov <venelin_bakalov@abv.bg>
2016-10-19 15:03:16 +0300

    Extract-Emails.csproj problem solved!!!

---
+++ Strings-And-RegEx-Exercise/Extract-Emails/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Extract_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string pattern = @"\s([0-9A-Za-z]+(-*|_*|\.*)[0-9a-zA-Z]*@[a-z]+-*[a-z]*(\.[a-z]+)+\b)";
+
+            Regex regexCheck = new Regex(pattern);
+
+            MatchCollection matches = regexCheck.Matches(text);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Groups[1]);
+            }
+            
+        }
+    }
+}
