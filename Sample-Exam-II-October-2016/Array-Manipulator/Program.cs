161cf253486a52b1c074277a407075bb94530e6f
venelinbakalov <venelin_bakalov@abv.bg>
2016-10-22 21:05:30 +0300

    added Array-Manipulator.csproj solution

---
+++ Sample-Exam-II-October-2016/Array-Manipulator/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Manipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            string command = Console.ReadLine().ToLower();
+
+            while (!command.Equals("end"))
+            {
+                string[] commandArgs = command.Split().ToArray();
+                switch (commandArgs[0])
+                {
+                    case "exchange":
+                        Exchange(commandArgs, numbers);
+                        break;
+
+                    case "max":
+                        PrintMax(commandArgs, numbers);
+                        break;
+                    case "min":
+                        PrintMin(commandArgs, numbers);
+                        break;
+                    case "first":
+                        PrintFirstElements(commandArgs, numbers);
+                        break;
+                    case "last":
+                        PrintLastElements(commandArgs, numbers);
+                        break;
+                }
+
+                command = Console.ReadLine().ToLower();
+            }
+
+            Console.WriteLine("[" + string.Join(", ", numbers) + "]");
+        }
+
+        private static void PrintLastElements(string[] commandArgs, List<int> numbers)
+        {
+            int count = int.Parse(commandArgs[1]);
+
+            if (count > numbers.Count || count < 0)
+            {
+                Console.WriteLine("Invalid count");
+            }
+            else
+            {
+                string type = commandArgs[2];
+                int remainder = type == "even" ? 0 : 1;
+
+                int[] elements = numbers.Where(x => Math.Abs(x) % 2 == remainder).Reverse().Take(count).Reverse().ToArray();
+                if (elements.Length.Equals(0))
+                {
+                    Console.WriteLine("[]");
+                }
+                else
+                {
+                    Console.WriteLine("[" + string.Join(", ", elements) + "]");
+                }
+            }
+        }
+        private static void PrintFirstElements(string[] commandArgs, List<int> numbers)
+        {
+            int count = int.Parse(commandArgs[1]);
+
+            if (count > numbers.Count || count < 0)
+            {
+                Console.WriteLine("Invalid count");
+            }
+            else
+            {
+                string type = commandArgs[2];
+                int remainder = type == "even" ? 0 : 1;
+                int[] elements = numbers.Where(x => Math.Abs(x) % 2 == remainder).Take(count).ToArray();
+                if (elements.Length.Equals(0))
+                {
+                    Console.WriteLine("[]");
+
+                }
+                else
+                {
+                    Console.WriteLine("[" + string.Join(", ", elements) + "]");
+                }
+            }
+        }
+
+        private static void PrintMin(string[] commandArgs, List<int> numbers)
+        {
+            string evenOrOdd = commandArgs[1];
+            if (evenOrOdd.Equals("even"))
+            {
+                try
+                {
+                    int min = numbers.Where(x => Math.Abs(x) % 2 == 0).Min();
+                    int minIndex = numbers.LastIndexOf(min);
+                    Console.WriteLine(minIndex);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("No matches");
+                }
+            }
+            else if (evenOrOdd.Equals("odd"))
+            {
+                try
+                {
+                    int min = numbers.Where(x => Math.Abs(x) % 2 == 1).Min();
+                    int minIndex = numbers.LastIndexOf(min);
+                    Console.WriteLine(minIndex);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("No matches");
+                }
+            }
+        }
+
+        private static void PrintMax(string[] commandArgs, List<int> numbers)
+        {
+            string evenOrOdd = commandArgs[1];
+            if (evenOrOdd.Equals("even"))
+            {
+                try
+                {
+                    int max = numbers.Where(x => Math.Abs(x) % 2 == 0).Max();
+                    int maxIndex = numbers.LastIndexOf(max);
+                    Console.WriteLine(maxIndex);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("No matches");
+                }
+
+            }
+            else if (evenOrOdd.Equals("odd"))
+            {
+                try
+                {
+                    int max = numbers.Where(x => Math.Abs(x) % 2 == 1).Max();
+                    int maxIndex = numbers.LastIndexOf(max);
+                    Console.WriteLine(maxIndex);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("No matches");
+                }
+            }
+        }
+
+        private static void Exchange(string[] commandArgs, List<int> numbers)
+        {
+            int index = int.Parse(commandArgs[1]);
+            if (index < 0 || index >= numbers.Count)
+            {
+                Console.WriteLine("Invalid index");
+            }
+            else
+            {
+                List<int> firstGroup = numbers.Take(index+1).ToList();
+                numbers.RemoveRange(0, index + 1);
+                numbers.AddRange(firstGroup);
+            }
+        }
+    }
+}
---
+++ Sample-Exam-II-October-2016/Cubic-Messages-with-Char.IsDigit/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cubic_Messages_with_Char.IsDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string encryptedMessage = Console.ReadLine();
+
+            while (!encryptedMessage.Equals("Over!"))
+            {
+                int count = int.Parse(Console.ReadLine());
+                Regex pattern = new Regex(@"^([0-9]+)([a-zA-Z]{" + count + @"})([^a-zA-Z]*)$");
+
+                Match match = pattern.Match(encryptedMessage);
+
+                if (match.Success)
+                {
+                    string message = match.Groups[2].ToString();
+                    Console.Write(message + " == ");
+                    List<int> indexes = GetIndexes(match);
+
+                    foreach (var i in indexes)
+                    {
+                        if (0 <= i && i < message.Length)
+                        {
+                            Console.Write(message[i]);
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+
+                    Console.WriteLine();
+                }
+
+                encryptedMessage = Console.ReadLine();
+            }
+
+        }
+
+        private static List<int> GetIndexes(Match match)
+        {
+            List<int> indexes = new List<int>();
+            string left = match.Groups[1].ToString();
+            for (int i = 0; i < left.Length; i++)
+            {
+                if (Char.IsDigit(left[i]))
+                {
+                    indexes.Add(int.Parse(left[i].ToString()));
+                }
+            }
+
+            string right = match.Groups[3].ToString();
+            for (int i = 0; i < right.Length; i++)
+            {
+                if (Char.IsDigit(right[i]))
+                {
+                    indexes.Add(int.Parse(right[i].ToString()));
+                }
+            }
+
+            return indexes;
+        }
+    }
+}
