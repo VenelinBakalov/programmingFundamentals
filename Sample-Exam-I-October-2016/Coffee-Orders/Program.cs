57bc96bec8c8b0621df2866ca2bee0efed81003e
Venelin Bakalov <venelin_bakalov@abv.bg>
2017-01-03 23:50:44 +0200

    fixed problem with missing Sample-Exam-I-October-2016.sln

---
+++ Sample-Exam-I-October-2016/Coffee-Orders/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coffee_Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            decimal totalPrice = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                decimal price = decimal.Parse(Console.ReadLine());
+                DateTime orderDate = DateTime.ParseExact(Console.ReadLine(),
+                    "d/M/yyyy", CultureInfo.InvariantCulture);
+                long capsulesCount = long.Parse(Console.ReadLine());
+
+                decimal coffeePrice = (DateTime.DaysInMonth(orderDate.Year, orderDate.Month) * capsulesCount) * price;
+                totalPrice += coffeePrice;
+                Console.WriteLine($"The price for the coffee is: ${coffeePrice:f2}");
+            }
+
+            Console.WriteLine($"Total: ${totalPrice:f2}");
+        }
+    }
+}
---
+++ Sample-Exam-I-October-2016/Rage-Quit/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Rage_Quit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToUpper();
+            string pattern = "(\\D+)(\\d+)";
+
+            StringBuilder output = new StringBuilder();
+
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                string symbols = match.Groups[1].ToString();
+                int count = int.Parse(match.Groups[2].ToString());
+
+                for (int i = 0; i < count; i++)
+                {
+                    output.Append(symbols);
+                }
+            }
+
+            int uniqueSymbols = output.ToString().Distinct().Count();
+
+            Console.WriteLine($"Unique symbols used: {uniqueSymbols}");
+            Console.WriteLine(output);
+        }
+    }
+}
